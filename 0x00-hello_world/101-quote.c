02c62611b4bc72e43975a632099830e369af2bdd
azzasouei <azzasouei@gmail.com>
2023-07-13 08:35:03 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ *main - Entry point
+ *Description: print a script using write function
+ *ssize_t write(int fd, const void *buf, size_t count);
+ *Return: 1 (Failed).
+*/
+int main(void)
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
+	return (1);
+}
