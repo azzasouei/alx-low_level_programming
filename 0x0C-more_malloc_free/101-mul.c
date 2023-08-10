50dbf49793ba52c324ba7413e06553ed6fecd626
azzasouei <azzasouei@gmail.com>
2023-08-10 08:19:53 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,88 +1,102 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 
+#define ERR_MSG "Error"
 
 /**
- * _puts - prints a string, followed by a new line
- * @str: pointer to the string to print
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-void _puts(char *str)
+int is_digit(char *s)
 {
-	while (*str)
+	int i = 0;
+
+	while (s[i])
 	{
-		putchar(*str);
-		str++;
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
-	putchar('\n');
+	return (1);
 }
 
 /**
- * _is_positive_number - checks if a string is composed of digits and positive
- * @s: pointer to the string to check
- * Return: 1 if s is a positive number, 0 otherwise
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
-int _is_positive_number(char *s)
+int _strlen(char *s)
 {
-	while (*s)
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (!_isdigit(*s))
-		{
-			return 0;
-		}
-		s++;
+		i++;
 	}
-	return 1;
+	return (i);
 }
 
 /**
- * _atoi - convert a string to an integer
- * @s: pointer to the string to convert
- * Return: integer converted
+ * errors - handles errors for main
  */
-int _atoi(const char *s)
+void errors(void)
 {
-	int sign = 1;
-	unsigned long int resp = 0, i = 0;
-
-	if (*s == '-')
-	{
-		sign = -1;
-		s++;
-	}
-
-	while (*s)
-	{
-		if (!_isdigit(*s))
-		{
-			_puts("Error");
-			exit(98);
-		}
-		resp = resp * 10 + (*s - '0');
-		s++;
-	}
-
-	return (sign * resp);
+	printf("Error\n");
+	exit(98);
 }
 
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 on success, 98 on error
+ *
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3 || !_is_positive_number(argv[1]) || !_is_positive_number(argv[2]))
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		_puts("Error");
-		return (98);
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	int num1 = _atoi(argv[1]);
-	int num2 = _atoi(argv[2]);
-	int result = num1 * num2;
-
-	printf("%d\n", result);
-
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
+
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,7 +9,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-void _puts(char *str);
-int _atoi(const char *s);
-int _is_positive_number(char *s)
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 #endif /* MAIN_H */
