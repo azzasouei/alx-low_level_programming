083dac09933143418dbb711f55a8270f4048d50a
azzasouei <azzasouei@gmail.com>
2023-08-14 05:44:46 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * Description: change the name of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
